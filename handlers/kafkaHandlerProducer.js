8da0b3e23bfb00234d7ea79110377c942876eabc
ElielGez <eliel7114@gmail.com>
2021-07-23 18:20:01 +0300

    add id

--- handlers/kafkaHandlerProducer.js
+++ handlers/kafkaHandlerProducer.js
@@ -3,31 +3,32 @@ const readConfFile = require("../conf/conf")
 
 const conf = readConfFile()
 
-const createRandomData = () =>{
+const createRandomData = (vehicleId) => {
     const options = conf.data
     const data = {
-        type :options.type[Math.floor(Math.random()*options.type.length)],
-        section:options.section[Math.floor(Math.random()*options.section.length)],
-        vehicleType:options.vehicleType[Math.floor(Math.random()*options.vehicleType.length)],
-        dayOfWeek:options.dayOfWeek[Math.floor(Math.random()*options.dayOfWeek.length)],
-        hour:options.hour[Math.floor(Math.random()*options.hour.length)],
-        dayType:options.dayType[Math.floor(Math.random()*options.dayType.length)],
+        vehicleId,
+        type: options.type[Math.floor(Math.random() * options.type.length)],
+        section: options.section[Math.floor(Math.random() * options.section.length)],
+        vehicleType: options.vehicleType[Math.floor(Math.random() * options.vehicleType.length)],
+        dayOfWeek: options.dayOfWeek[Math.floor(Math.random() * options.dayOfWeek.length)],
+        hour: options.hour[Math.floor(Math.random() * options.hour.length)],
+        dayType: options.dayType[Math.floor(Math.random() * options.dayType.length)],
     }
 
     return data;
 }
 
-const afterConnect = ()=>{
+const afterConnect = () => {
     console.log("Producer connected to kafka!");
-    for(let i=0;i<10;i++){
-     kafkaProducer.sendMessage(conf.dataTopic,createRandomData())
+    for (let i = 0; i < 10; i++) {
+        kafkaProducer.sendMessage(conf.dataTopic, createRandomData(i + 1))
     }
 }
 
 const kafkaProducer = new KafkaProducerClient(
     conf.prefix,
     afterConnect,
-    (topic,msgAsJson)=>console.log(`Push to ${topic} this message: ${JSON.stringify(msgAsJson)}`)
+    (topic, msgAsJson) => console.log(`Push to ${topic} this message: ${JSON.stringify(msgAsJson)}`)
 )
 
 kafkaProducer.connect()
