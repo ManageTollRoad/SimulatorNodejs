f59c6ab409fa7dce0d8d7af7bf0df2a406d767db
Ofekroz10 <ofek@lightricks.com>
2021-07-30 15:58:58 +0300

    round 2

--- handlers/kafkaHandlerProducer.js
+++ handlers/kafkaHandlerProducer.js
@@ -8,28 +8,39 @@ const createRandomData = (vehicleId) => {
     const data = {
         vehicleId,
         type: options.type[Math.floor(Math.random() * options.type.length)],
-        section: options.section[Math.floor(Math.random() * options.section.length)],
         vehicleType: options.vehicleType[Math.floor(Math.random() * options.vehicleType.length)],
         dayOfWeek: options.dayOfWeek[Math.floor(Math.random() * options.dayOfWeek.length)],
         hour: options.hour[Math.floor(Math.random() * options.hour.length)],
         dayType: options.dayType[Math.floor(Math.random() * options.dayType.length)],
+        section: options.section[Math.floor(Math.random() * options.section.length)],
     }
 
     return data;
 }
 
-const afterConnect = () => {
+const postData = () => {
     console.log("Producer connected to kafka!");
     for (let i = 0; i < 10; i++) {
         kafkaProducer.sendMessage(conf.dataTopic, createRandomData(i + 1))
     }
 }
 
+const postTrain = () => {
+    console.log("Producer connected to kafka for training!");
+    for (let i = 0; i < 200; i++) {
+        kafkaProducer.sendMessage(conf.trainTopic, createRandomData(i + 1))
+    }
+}
+
+// for train make it true, for data make it false
+const isTrain = false;
+
 const kafkaProducer = new KafkaProducerClient(
     conf.prefix,
-    afterConnect,
+    isTrain? postTrain : postData,
     (topic, msgAsJson) => console.log(`Push to ${topic} this message: ${JSON.stringify(msgAsJson)}`)
 )
+
 
 kafkaProducer.connect()
 
